92e1f0465122fbd781b74b87c701ef8ca4469367
Jason Zheng <jasonzheng0814@gmail.com>
2025-01-27 19:25:47 -0500

    create initial functions for calculator

---
+++ calculate-functions.js
@@ -0,0 +1,25 @@
+let numberOne;
+let operator;
+let numberTwo;
+
+function add(a,b) {
+    
+}
+
+function subtract(a,b) {
+    
+}
+
+function multiply(a,b) {
+    
+}
+
+function subtract(a,b) {
+    
+}
+
+function operate() {
+    
+
+}
+
