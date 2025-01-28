39027a6cd69afa676b31474e9b5125eacaac4c8e
Jason Zheng <jasonzheng0814@gmail.com>
2025-01-27 19:37:10 -0500

    add initial clear function

--- calculate-functions.js
+++ calculate-functions.js
@@ -19,7 +19,16 @@ function subtract(a,b) {
 }
 
 function operate() {
-    
+    equationContent = document.querySelector(".calculation-text");
+    equation = equationContent.textContent;
+    console.log(equation);
+}
 
+function clear() {
+    equationContent = document.querySelector(".calculation-text");
+    equationContent.textContent = "";
+    numberOne = NaN;
+    operator = "";
+    numberTwo = NaN;
 }
 
