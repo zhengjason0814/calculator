73a2e6711b51b9f499d67f6eebf654136020f574
Jason Zheng <jasonzheng0814@gmail.com>
2025-02-10 04:50:44 -0500

    clear reset flawed logic

--- calculate-functions.js
+++ calculate-functions.js
@@ -1,34 +1,113 @@
-let numberOne;
+let workingValue;
+let totalValue;
 let operator;
-let numberTwo;
 
 function add(a,b) {
-    
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a,b) {
-    
+    return parseInt(a) - parseInt(b);
+}
+
+function divide(a,b) {
+    return parseInt(a) / parseInt(b);
 }
 
 function multiply(a,b) {
-    
-}
-
-function subtract(a,b) {
-    
+    return parseInt(a) * parseInt(b);
 }
 
 function operate() {
-    equationContent = document.querySelector(".calculation-text");
-    equation = equationContent.textContent;
     console.log(equation);
 }
 
 function clear() {
-    equationContent = document.querySelector(".calculation-text");
-    equationContent.textContent = "";
-    numberOne = NaN;
+    equation.textContent = "";
+    workingValue = "";
     operator = "";
-    numberTwo = NaN;
+    totalValue = "";
 }
 
+equation = document.querySelector(".calculation-text");
+
+numberButtons = document.querySelector(".number-section");
+numberButtons.addEventListener('click', (e) => {
+    let target = e.target;
+
+    switch(target.id) {
+        case "zero":
+            equation.textContent += "0";
+            break;
+        case "one":
+            equation.textContent += "1";
+            break;
+        case "two":
+            equation.textContent += "2";
+            break;
+        case "three":
+            equation.textContent += "3";
+            break;
+        case "four":
+            equation.textContent += "4";
+            break;
+        case "five":
+            equation.textContent += "5";
+            break;
+        case "six":
+            equation.textContent += "6";
+            break;
+        case "seven":
+            equation.textContent += "7";
+            break;
+        case "eight":
+            equation.textContent += "8";
+            break;
+        case "nine":
+            equation.textContent += "9";
+            break;
+        case "clear":
+            clear();
+            break;
+    }
+});
+
+operatorButtons = document.querySelector(".operator-section");
+operatorButtons.addEventListener('click', (e) => {
+    let target = e.target;
+
+    switch(target.id) {
+        case "plus":
+            operator = "+";
+            break;
+        case "minus":
+            operator = "-";
+            break;
+        case "divide":
+            operator = "/";
+            break;
+        case "multiply":
+            operator = "*";
+            break;
+        case "equals":
+            switch (operator) {
+                case "+":
+                    workingValue = add(workingValue,tempValue);
+                    break;
+                case "-":
+                    workingValue = subtract(workingValue,tempValue);
+                    break;
+                case "/":
+                    workingValue = divide(workingValue,tempValue);
+                    
+                    break;
+                case "*":
+                    workingValue = multiply(workingValue,tempValue);
+                    
+                    break;
+                default:
+                    break;
+            }
+            break;
+    }
+});
